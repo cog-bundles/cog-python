d0cdca1e560d8906871a696eab8e3c59d461e523
Kevin Smith <kevin@operable.io>
2016-02-12 15:49:49 -0500

    Fine grained logging

---
+++ cog/logger.py
@@ -0,0 +1,40 @@
+from __future__ import print_function
+
+import sys
+
+class Logger(object):
+    Debug, Info, Warn, Error = range(4)
+
+    @classmethod
+    def _write_log_message(cls, message, level):
+        prefix = "COGCMD_"
+        if level == Logger.Debug:
+            prefix = prefix + "DEBUG"
+        elif level == Logger.Info:
+            prefix = prefix + "INFO"
+        elif level == Logger.Warn:
+            prefix = prefix + "WARN"
+        else:
+            prefix = prefix + "ERR"
+        print("%s:%s" % (prefix, message), end="\n", file=sys.stdout)
+        sys.stdout.flush()
+
+    @classmethod
+    def log(cls, message, level):
+        Logger._write_log_message(message, level)
+
+    @classmethod
+    def debug(cls, message):
+        Logger.log(message, Logger.Debug)
+
+    @classmethod
+    def info(cls, message):
+        Logger.log(message, Logger.Info)
+
+    @classmethod
+    def warn(cls, message):
+        Logger.log(message, Logger.Warn)
+
+    @classmethod
+    def error(cls, message):
+        Logger.log(message, Logger.Error)
--- cog/output.py
+++ cog/output.py
@@ -2,13 +2,6 @@ from __future__ import print_function
 import json
 import sys
 
-def log(message, error=False):
-    fd = sys.stdout
-    if error:
-        fd = sys.stderr
-    print("LOG: %s" % (message), end="\n", file=fd)
-    fd.flush()
-
 def send_json(data, fd=sys.stdout):
     print("JSON", end="\n", file=fd)
     print(json.dumps(data), end="\n", file=fd)
